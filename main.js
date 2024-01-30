2e8cb260038294efa782fda9aa332cccd62e0ab0
Lakshitachouhan <chouhanlakshita18@gmail.com>
2024-01-31 02:40:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper("#one", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+});
+
+var swiper = new Swiper("#two", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+
+
+
+
+  
